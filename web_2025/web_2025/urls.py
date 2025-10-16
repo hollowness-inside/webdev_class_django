e76fb761afb136beea235dd658f09f0d0ba33783
hollowness-inside <malikov.vp@students.dvfu.ru>
2025-10-16 13:32:00 +1000

    Redirect index to fefu_lab

--- web_2025/web_2025/urls.py
+++ web_2025/web_2025/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import fefu_lab
+
 urlpatterns = [
-    path("fefu_lab/", include("fefu_lab.urls")),
+    path("", include("fefu_lab.urls")),
     path('admin/', admin.site.urls),\
 ]
