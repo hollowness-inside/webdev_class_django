a559bf8d2b331654924e3fa73dc5cb80a7e526d9
hollowness-inside <malikov.vp@students.dvfu.ru>
2025-10-16 13:31:34 +1000

    Create StudentProfileView

--- web_2025/fefu_lab/urls.py
+++ web_2025/fefu_lab/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
 from . import views
+from .views import StudentProfileView
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path("", views.index, name="index"),
+    path("student/<int:student_id>/",
+         StudentProfileView.as_view(), name="student_profile")
 ]
--- web_2025/fefu_lab/views.py
+++ web_2025/fefu_lab/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, World from a function-based view")
+
+
+class StudentProfileView(View):
+    def get(self, request, *args, **kwargs):
+        student_id = self.kwargs.get('student_id')
+
+        if student_id > 100:
+            return Http404("Student not found")
+
+        return HttpResponse(f"Student profile with id {student_id}")
