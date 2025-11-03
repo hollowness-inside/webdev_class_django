9940ab16e945aa0d3fc027cde8eaf75a772cded1
hollowness-inside <malikov.vp@students.dvfu.ru>
2025-11-03 14:25:06 +1000

    Update the index page

--- web_2025/web_2025/settings.py
+++ web_2025/web_2025/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hppszjg@vgcp*f7dcvambs5^^jz0tso+=ja@u=*++5sdjqt-3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
