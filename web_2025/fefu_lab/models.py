987c69b449ac670c59b3790f1add5844247e4255
hollowness-inside <malikov.vp@students.dvfu.ru>
2025-11-03 18:24:04 +1000

    class UserProfile model

--- web_2025/fefu_lab/models.py
+++ web_2025/fefu_lab/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=30)
+    clan = models.CharField(max_length=30)
+    status = models.CharField(max_length=25)
+    year = models.IntegerField()
